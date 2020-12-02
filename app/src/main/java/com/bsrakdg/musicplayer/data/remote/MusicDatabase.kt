e50499e48265562e43b7846d3c5b0ab9f4389c21
bsrAkdg <busraaakdag@gmail.com>
2020-12-02 12:44:52 +0300

    Firebase database setup

---
+++ app/src/main/java/com/bsrakdg/musicplayer/data/remote/MusicDatabase.kt
@@ -0,0 +1,21 @@
+package com.bsrakdg.musicplayer.data.remote
+
+import com.bsrakdg.musicplayer.data.entities.Song
+import com.bsrakdg.musicplayer.other.Constants.SONG_COLLECTION
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+
+class MusicDatabase {
+
+    private val fireStore = FirebaseFirestore.getInstance()
+    private val songCollection = fireStore.collection(SONG_COLLECTION)
+
+    suspend fun getAllSongs(): List<Song> {
+        return try {
+            songCollection.get().await().toObjects(Song::class.java)
+        } catch (exception: Exception) {
+            emptyList()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/bsrakdg/musicplayer/other/Constants.kt
@@ -0,0 +1,6 @@
+package com.bsrakdg.musicplayer.other
+
+object Constants {
+
+    const val SONG_COLLECTION = "songs"
+}

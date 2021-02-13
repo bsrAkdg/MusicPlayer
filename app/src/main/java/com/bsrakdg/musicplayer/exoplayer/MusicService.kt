f23f548916438e96fcd0dca0c163763887c74c0e
bserdaroglu <busraaakdag@gmail.com>
2021-02-13 15:22:29 +0300

    Setting up the music notification

--- app/src/main/java/com/bsrakdg/musicplayer/exoplayer/MusicService.kt
+++ app/src/main/java/com/bsrakdg/musicplayer/exoplayer/MusicService.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
 import android.support.v4.media.session.MediaSessionCompat
 import androidx.media.MediaBrowserServiceCompat
+import com.bsrakdg.musicplayer.exoplayer.callbacks.MusicPlayerNotificationListener
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
@@ -35,6 +36,10 @@ class MusicService : MediaBrowserServiceCompat() {
     private lateinit var mediaSession : MediaSessionCompat
     private lateinit var mediaSessionConnector: MediaSessionConnector
 
+    var isForegroundService = false
+
+    private lateinit var musicNotificationManager: MusicNotificationManager
+
     override fun onCreate() {
         super.onCreate()
 
@@ -53,6 +58,15 @@ class MusicService : MediaBrowserServiceCompat() {
         // Represents an ongoing session.
         // We need to assign the token of our just created media session to our service (MusicService)
         sessionToken = mediaSession.sessionToken
+
+        // create notification manager for handling notification state
+        musicNotificationManager = MusicNotificationManager(
+            this,
+            mediaSession.sessionToken,
+            MusicPlayerNotificationListener(this)
+        ) {
+
+        }
 
         mediaSessionConnector = MediaSessionConnector(mediaSession)
         mediaSessionConnector.setPlayer(exoPlayer)
--- app/src/main/java/com/bsrakdg/musicplayer/other/Constants.kt
+++ app/src/main/java/com/bsrakdg/musicplayer/other/Constants.kt
@@ -5,4 +5,7 @@ object Constants {
     const val SONG_COLLECTION = "songs"
 
     const val USER_AGENT = "Music Player App"
+
+    const val NOTIFICATION_CHANNEL_ID = "music"
+    const val NOTIFICATION_ID = 1
 }

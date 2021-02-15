4cf20fca87befa7964c5a08817ba55eb77db0096
bserdaroglu <busraaakdag@gmail.com>
2021-02-15 22:08:37 +0300

    Toggling the Play State

--- app/src/main/java/com/bsrakdg/musicplayer/ui/MainActivity.kt
+++ app/src/main/java/com/bsrakdg/musicplayer/ui/MainActivity.kt
@@ -2,14 +2,19 @@ package com.bsrakdg.musicplayer.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.media.session.PlaybackStateCompat
 import androidx.activity.viewModels
+import androidx.viewpager2.widget.ViewPager2
+import com.bsrakdg.musicplayer.R
 import com.bsrakdg.musicplayer.adapters.SwipeSongAdapter
 import com.bsrakdg.musicplayer.data.entities.Song
 import com.bsrakdg.musicplayer.databinding.ActivityMainBinding
+import com.bsrakdg.musicplayer.exoplayer.isPlaying
 import com.bsrakdg.musicplayer.exoplayer.toSong
 import com.bsrakdg.musicplayer.other.Status.*
 import com.bsrakdg.musicplayer.ui.viewmodel.MainViewModel
 import com.bumptech.glide.RequestManager
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -28,6 +33,8 @@ class MainActivity : AppCompatActivity() {
 
     private var curPlayingSong: Song? = null
 
+    private var playbackState: PlaybackStateCompat? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -36,6 +43,21 @@ class MainActivity : AppCompatActivity() {
         subscribeToObservers()
 
         binding.vpSong.adapter = swipeSongAdapter
+        binding.vpSong.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                if (playbackState?.isPlaying == true) {
+                    mainViewModel.playOrToggleSong(swipeSongAdapter.currentList[position])
+                } else {
+                    curPlayingSong = swipeSongAdapter.currentList[position]
+                }
+            }
+        })
+        binding.ivPlayPause.setOnClickListener {
+            curPlayingSong?.let { song ->
+                mainViewModel.playOrToggleSong(song, true)
+            }
+        }
     }
 
     private fun switchViewPagerToCurrentSong(song: Song) {
@@ -65,13 +87,46 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-        
+
         mainViewModel.curPlayingSong.observe(this) { mediaDataCompat ->
             if (mediaDataCompat == null) return@observe
 
             curPlayingSong = mediaDataCompat.toSong()
             glide.load(curPlayingSong?.imageUrl).into(binding.ivCurSongImage)
             switchViewPagerToCurrentSong(curPlayingSong ?: return@observe)
+        }
+
+        mainViewModel.playbackState.observe(this) {
+            playbackState = it
+            binding.ivPlayPause.setImageResource(
+                if (playbackState?.isPlaying == true) R.drawable.ic_pause else R.drawable.ic_play
+            )
+        }
+
+        mainViewModel.isConnected.observe(this) {
+            it.getContentIfNotHandled()?.let { result ->
+                when (result.status) {
+                    ERROR -> Snackbar.make(
+                        binding.rootLayout,
+                        result.message ?: "An unknown error occurred",
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                    else -> Unit
+                }
+            }
+        }
+
+        mainViewModel.networkError.observe(this) {
+            it.getContentIfNotHandled()?.let { result ->
+                when (result.status) {
+                    ERROR -> Snackbar.make(
+                        binding.rootLayout,
+                        result.message ?: "An unknown error occurred",
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                    else -> Unit
+                }
+            }
         }
     }
 }
